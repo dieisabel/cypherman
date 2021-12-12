26fedeedd8887bf8fe2b681b00cc7f9d150023c9
Konstantine <konstkadyr@gmail.com>
2021-12-12 18:47:20 +0200

    Add sha256 hashing algorithm

--- src/app/entities/hashing_algorithms/__init__.py
+++ src/app/entities/hashing_algorithms/__init__.py
@@ -2,8 +2,10 @@ __all__ = [
     'IHashingAlgorithm',
     'MD5HashingAlgorithm',
     'SHA1HashingAlgorithm',
+    'SHA256HashingAlgorithm',
 ]
 
 from entities.hashing_algorithms.hashing_algorithm import IHashingAlgorithm
 from entities.hashing_algorithms.md5 import MD5HashingAlgorithm
 from entities.hashing_algorithms.sha1 import SHA1HashingAlgorithm
+from entities.hashing_algorithms.sha256 import SHA256HashingAlgorithm
