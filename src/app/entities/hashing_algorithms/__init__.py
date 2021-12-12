74b8326f93626559337967fe69adccf5628684ac
Konstantine <konstkadyr@gmail.com>
2021-12-12 17:09:16 +0200

    Add md5 hashing algorithm

--- src/app/entities/hashing_algorithms/__init__.py
+++ src/app/entities/hashing_algorithms/__init__.py
@@ -1,5 +1,7 @@
 __all__ = [
     'IHashingAlgorithm',
+    'MD5HashingAlgorithm',
 ]
 
 from entities.hashing_algorithms.hashing_algorithm import IHashingAlgorithm
+from entities.hashing_algorithms.md5 import MD5HashingAlgorithm
