0ae625de3d2567d457fb70c66a337446b43b4bc5
Konstantine <konstkadyr@gmail.com>
2021-12-13 16:39:47 +0200

    Add tests for sha1 algorithm

--- src/tests/entities/hashing_algorithms/conftest.py
+++ src/tests/entities/hashing_algorithms/conftest.py
@@ -1,8 +1,17 @@
 import pytest
 
-from entities.hashing_algorithms import IHashingAlgorithm, MD5HashingAlgorithm
+from entities.hashing_algorithms import (
+    IHashingAlgorithm,
+    MD5HashingAlgorithm,
+    SHA1HashingAlgorithm,
+)
 
 
 @pytest.fixture
 def md5() -> IHashingAlgorithm:
     return MD5HashingAlgorithm()
+
+
+@pytest.fixture
+def sha1() -> IHashingAlgorithm:
+    return SHA1HashingAlgorithm()
