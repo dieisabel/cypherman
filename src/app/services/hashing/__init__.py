be44cd3ad341a45645cb5f96fa0ada0bdc10f2af
Konstantine <konstkadyr@gmail.com>
2021-12-20 14:56:43 +0200

    Add hashing service implementation

--- src/app/services/hashing/__init__.py
+++ src/app/services/hashing/__init__.py
@@ -1,5 +1,7 @@
 __all__ = [
     'IHashingService',
+    'HashingService',
 ]
 
 from services.hashing.ihashing_service import IHashingService
+from services.hashing.hashing_service import HashingService
