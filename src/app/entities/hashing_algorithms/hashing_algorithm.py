76494ab65e575e282d0f9729eefb160bfb512d03
Konstantine <konstkadyr@gmail.com>
2021-12-22 20:51:17 +0200

    Enhance docs for hashing algorithm

--- src/app/entities/hashing_algorithms/hashing_algorithm.py
+++ src/app/entities/hashing_algorithms/hashing_algorithm.py
@@ -4,6 +4,9 @@ from abc import ABC, abstractmethod
 
 
 class IHashingAlgorithm(ABC):
+    name: str
+    bits: int
+    is_secure: bool
 
     # TODO: Should we use checksum property instead of hash method?
     @abstractmethod
