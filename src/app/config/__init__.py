f3f4de0cf1bc22349308b00609dd0a44df6e63c9
Kostiantyn <konstkadyr@gmail.com>
2021-12-22 19:31:23 +0200

    Create basic config (#34)
    * Add config package
    
    * Add base settings
    
    * Add .env example file

---
+++ src/app/config/__init__.py
@@ -0,0 +1,6 @@
+__all__ = [
+    'Settings',
+    'get_settings',
+]
+
+from config.settings import Settings, get_settings
---
+++ src/app/config/settings.py
@@ -0,0 +1,22 @@
+__all__ = [
+    'Settings',
+    'get_settings',
+]
+
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    APPLICATION_NAME: str = 'Cypherman'
+    APPLICATION_DESCRIPTION: str = 'Secure your data with Cypherman!'
+    VERSION: str = '0.0.0'
+
+    class Config:
+        env_file = '.env'
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    return Settings()
