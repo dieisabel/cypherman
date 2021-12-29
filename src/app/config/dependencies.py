b5cad863431384b0378b9c5e1a8b669f110ed5ba
Konstantine <konstkadyr@gmail.com>
2021-12-29 23:03:15 +0200

    Add custom exception

--- src/app/config/dependencies.py
+++ src/app/config/dependencies.py
@@ -7,13 +7,14 @@ import os
 from config.base import Config
 from config.development import DevelopmentConfig
 from config.testing import TestingConfig
+from config.exceptions import ImproperlyConfigured
 
 
 @lru_cache()
 def get_config() -> Config:
     config_environ: str = os.environ.get('FASTAPI_CONFIGURATION')
     if not config_environ:
-        raise Exception('Application configuration is not set')
+        raise ImproperlyConfigured('Application configuration is not set')
 
     config_registry: Dict[str, Type[Config]] = {
         'development': DevelopmentConfig,
@@ -21,6 +22,6 @@ def get_config() -> Config:
     }
     config_class: Optional[Type[Config]] = config_registry.get(config_environ.lower())
     if not config_class:
-        raise Exception('Config not found')
+        raise ImproperlyConfigured('Config not found')
 
     return config_class()
