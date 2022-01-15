c1d45fd8a8e02a9459cf608f7e7664db42a46172
Konstantine <konstkadyr@gmail.com>
2022-01-15 22:25:03 +0200

    Call start_mappers while appliction creation

--- src/app/main.py
+++ src/app/main.py
@@ -4,8 +4,11 @@ from fastapi import FastAPI
 
 from api import router
 from core.config import get_config
+from core.db import start_mappers
 
 settings = get_config()
+
+start_mappers()
 
 application = FastAPI(
     title=settings.APPLICATION_NAME,
