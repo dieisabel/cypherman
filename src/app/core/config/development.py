2b1ffa76fa3916822e4bea5e2eec57cc5dbf821c
Kostiantyn <konstkadyr@gmail.com>
2022-03-11 13:51:00 +0000

    Configure docker and docker-compose for development environment (#77)
    * Add .dockerignore file
    
    * Add dockerfile for application
    
    * Add docker-compose
    
    * Configure task
    
    * Update config

--- src/app/core/config/development.py
+++ src/app/core/config/development.py
@@ -2,14 +2,13 @@
 
 __all__ = ['DevelopmentConfig']
 
+import os
+
 from core.config.base import BaseConfig
 
 
 class DevelopmentConfig(BaseConfig):
     """Settings for development environment"""
 
-    SQLALCHEMY_DATABASE_URL: str
+    SQLALCHEMY_DATABASE_URL: str = os.environ['SQLALCHEMY_DATABASE_URL']
     SQLALCHEMY_ECHO: bool = True
-
-    class Config:
-        env_file = '.env.development'
