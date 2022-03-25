32deb0f1730b6b8acaf5e8e3b131b41fe6320efb
Konstiantyn <konstkadyr@gmail.com>
2022-03-25 18:30:37 +0200

    Configure docker

--- src/app/core/config/testing.py
+++ src/app/core/config/testing.py
@@ -2,11 +2,13 @@
 
 __all__ = ['TestingConfig']
 
+import os
+
 from core.config.base import BaseConfig
 
 
 class TestingConfig(BaseConfig):
     """Settings for testing environment"""
 
-    SQLALCHEMY_DATABASE_URL: str = 'sqlite:///:memory:'
+    SQLALCHEMY_DATABASE_URL: str = os.environ.get('SQLALCHEMY_DATABASE_URL')
     SQLALCHEMY_ECHO: bool = False
